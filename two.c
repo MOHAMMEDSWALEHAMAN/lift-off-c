909dfd3b31e20553ae1c2420b07c59d8da537278
MOHAMMEDSWALEHAMAN <99548196+MOHAMMEDSWALEHAMAN@users.noreply.github.com>
2022-02-12 21:00:09 +0530

    Create two.c

---
+++ two.c
@@ -0,0 +1,17 @@
+/*program to find maximum between two numbers*/
+#include<stdio.h>
+int main()
+{ int a,b;
+printf("enter two numbers\n");
+scanf("%d%d" ,&a,&b);
+if(a>b)
+printf("%d is maximum number\n",a);
+else
+printf("%d is maximum number\n",b);
+
+
+
+
+
+
+}
