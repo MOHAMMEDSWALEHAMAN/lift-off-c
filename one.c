feab0ae8ffa20aba3502298f2ff3814900284709
MOHAMMEDSWALEHAMAN <99548196+MOHAMMEDSWALEHAMAN@users.noreply.github.com>
2022-02-12 20:58:59 +0530

    Create one.c

---
+++ one.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+printf ("name=mohammed swaleh aman\n");
+printf("course=btech\n");
+printf("stream=computer science\n");
+printf("my hobbies are playing call of duty mobile\n");
+
+
+
+
+}
