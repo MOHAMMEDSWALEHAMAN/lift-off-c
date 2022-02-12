33a4f6b5cf1e5f6e6c31dc003e1f930d462dbc05
MOHAMMEDSWALEHAMAN <99548196+MOHAMMEDSWALEHAMAN@users.noreply.github.com>
2022-02-12 21:07:25 +0530

    Create five.c

---
+++ five.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+float r,a,b,c,pi=3.14;
+printf("enter radius of circle");
+scanf("%f",&r);
+a=(pi*r*r);
+b=(2*pi*r);
+c=(2*r);
+printf("area=%f\n",a);
+printf("perimeter is %f\n",b);
+printf("diameter=%f\n",c);
+
+
+
+
+
+}
