be473e3f1e24edcebe5098334b79a0e4063c33fc
MOHAMMEDSWALEHAMAN <99548196+MOHAMMEDSWALEHAMAN@users.noreply.github.com>
2022-02-12 21:03:24 +0530

    Create four.c

---
+++ four.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+printf("enter two numbers\n");
+scanf("%d" ,&a);
+scanf("%d",&b);
+printf("sum is %d \n",c=a+b);
+printf("minus is %d \n",d=a-b);
+printf("multiply is %d \n",e=a*b);
+printf("division is %d \n",a/b);
+
+
+
+
+
+}
